ec33dfbd99254b1255e47ef5467c073d41f51529
Renato <renatommps@gmail.com>
2018-06-17 17:41:51 -0300

    tela de vídeos
    Implementação parcial da tela

--- app/src/main/java/com/example/appcopa2018/DisplayVideosActivity.java
+++ app/src/main/java/com/example/appcopa2018/DisplayVideosActivity.java
@@ -1,13 +1,108 @@
 package com.example.appcopa2018;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class DisplayVideosActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class DisplayVideosActivity extends YouTubeBaseActivity implements OnInitializedListener {
+
+    public static final String API_KEY = "AIzaSyD9YG6QcZU1L8nC1eRuzq36NeE4FW3jXfs";
+
+    public static final String VIDEO_ID = "tkBeAAVlxSM";
+
+    private PlayerStateChangeListener playerStateChangeListener = new PlayerStateChangeListener(){
+
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+
+        }
+
+        @Override
+        public void onVideoStarted() {
+
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
+
+    private PlaybackEventListener playbackEventListener = new PlaybackEventListener(){
+
+        @Override
+        public void onPlaying() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_videos);
+
+        // inicializa o YouTube Player View
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        youTubePlayerView.initialize(API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        // adiciona listeners para a instancia do YouTubePlayer
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
+        // começa a bufferizar
+        if (!wasRestored) {
+            youTubePlayer.cueVideo(VIDEO_ID);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Falhou ao iniciar o YouTube Player!", Toast.LENGTH_LONG).show();
     }
 }
